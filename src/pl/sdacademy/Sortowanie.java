e7afe91205e0e62f24aa3c8fb8e723260d1851a2
MartaMG <marta.mazij.garstka@gmail.com>
2018-03-11 15:35:51 +0100

    soortowanie bąbelkowe, Fibonacci, sortowanie karciane

--- src/pl/sdacademy/Sortowanie.java
+++ src/pl/sdacademy/Sortowanie.java
@@ -1,4 +1,29 @@
 package pl.sdacademy;
 
 public class Sortowanie {
-}
+    public static void main(String[] args) {
+        int[] tablica ={5,15,2,11,6,1,70,9};
+        bubbleSort(tablica);
+
+    }
+    public static void bubbleSort(int[] tablica){
+        int temp;
+        int zmiana = 1;
+        while(zmiana > 0){
+            zmiana = 0;
+            for(int i=0; i<tablica.length-1; i++){
+                if(tablica[i]>tablica[i+1]){
+                    temp = tablica[i+1];
+                    tablica[i+1] = tablica[i];
+                    tablica[i] = temp;
+                    zmiana++;
+                }
+            }
+        }
+        for(int i=0; i<tablica.length; i++){
+            System.out.print(tablica[i]+" ");
+        }
+    }
+    }
+
+
---
+++ src/pl/sdacademy/SortowanieKarciane.java
@@ -0,0 +1,11 @@
+package pl.sdacademy;
+
+public class SortowanieKarciane {
+    public static void main(String[] args) {
+
+    }
+    public static void liczby(){
+
+    }
+
+}
